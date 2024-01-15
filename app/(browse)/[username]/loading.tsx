0ab3d9eaa99a8de876cb630268dcb5000a4fff67
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2024-01-15 21:36:11 +0700

    Completed app

---
+++ app/(browse)/[username]/loading.tsx
@@ -0,0 +1,7 @@
+// import { StreamPlayerSkeleton } from "@/components/stream-player";
+
+const UserLoading = () => {
+  return <div className="h-full">{/* <StreamPlayerSkeleton /> */}</div>;
+};
+
+export default UserLoading;
